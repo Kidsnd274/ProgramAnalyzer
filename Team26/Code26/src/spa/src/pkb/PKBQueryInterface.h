677b3f5eff7ceabd619ed409f9f900d696b3fa33
DESKTOP-LGFS4RO\QMS <e0316059@u.nus.edu>
2022-08-30 17:28:01 +0800

    initiate pkd

---
+++ Team26/Code26/src/spa/src/pkb/PKBQueryInterface.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+typedef short PROC;
+
+class PKB {
+public:
+	static bool getUses (/* Stmt stmtlineno, Var var */);
+};
