46c6f341da0d1df65c2e1e4435143469256d0792
bryans17 <77219972+bryans17@users.noreply.github.com>
2022-08-31 10:52:30 +0800

    Update Lexer Test Case

--- Team26/Code26/src/unit_testing/src/util/TestLexer.cpp
+++ Team26/Code26/src/unit_testing/src/util/TestLexer.cpp
@@ -1,14 +1,24 @@
 #include "util/Lexer.h"
 #include "catch.hpp"
 #include <vector>
-using namespace std;
 
-string validSimple = "procedure test {\n  read x;\n  x = x + 1;\n}";
+std::string validSimple = "procedure test {\n  read x;\n  x = x + 1;\n}";
 
-// TODO: Test case is outdated and doesn't test tokenize() with vector<Token>
-//TEST_CASE("Basic Lexer test on valid SIMPLE", "This is a test for a very small and basic SIMPLE program") {
-//    Lexer lexer(validSimple);
-//    vector<string> res = lexer.tokenize();
-//    vector<string> expected = {"procedure", "test", "{", "read", "x", ";", "x", "=", "x", "+", "1", ";", "}"};
-//    REQUIRE(res == expected);
-//}
+TEST_CASE("Basic Lexer test on valid SIMPLE", "This is a test for a very small and basic SIMPLE program") {
+    Lexer lexer(validSimple);
+    std::vector<Token> res = lexer.tokenize();
+    std::vector<Token> expected = {TokenTypeExtractor::createNonTerminal("procedure"),
+                                   TokenTypeExtractor::createNonTerminal("test"),
+                                   TokenTypeExtractor::createTerminal("{"),
+                                   TokenTypeExtractor::createNonTerminal("read"),
+                                   TokenTypeExtractor::createNonTerminal("x"),
+                                   TokenTypeExtractor::createTerminal(";"),
+                                   TokenTypeExtractor::createNonTerminal("x"),
+                                   TokenTypeExtractor::createTerminal("="),
+                                   TokenTypeExtractor::createNonTerminal("x"),
+                                   TokenTypeExtractor::createTerminal("+"),
+                                   TokenTypeExtractor::createConst("1"),
+                                   TokenTypeExtractor::createTerminal(";"),
+                                   TokenTypeExtractor::createTerminal("}")};
+    REQUIRE(res == expected);
+}
