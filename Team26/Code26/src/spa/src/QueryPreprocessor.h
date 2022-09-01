c6ea50429b3c9c6b76db7b20489d11abec390547
Feng Zhunyi <leofengbar@gmail.com>
2022-09-02 01:15:40 +0800

    Implement container and parser in QPS -- Zhunyi

---
+++ Team26/Code26/src/spa/src/QueryPreprocessor.h
@@ -0,0 +1,14 @@
+#include <utility>
+#include "Tokenizer.h"
+#include "QueryProcessorTypes.h"
+
+#ifndef SPA_QUERYPREPROCESSOR_H
+#define SPA_QUERYPREPROCESSOR_H
+
+#endif //SPA_QUERYPREPROCESSOR_H
+
+namespace QueryPreprocessor{
+    std::pair<EntityType, bool> mapEntity(Tokenization::Token token);
+    std::pair<RelationType, bool> mapRelation(Tokenization::Token token);
+    void parseToken(std::vector<Tokenization::Token> tokens);
+}
