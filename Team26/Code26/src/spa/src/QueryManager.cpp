e74109c53e0efd1b07b82d3e345a2c2d04ac316f
Feng Zhunyi <leofengbar@gmail.com>
2022-09-04 22:28:10 +0800

    remove main functions in QPS

--- Team26/Code26/src/spa/src/QueryManager.cpp
+++ Team26/Code26/src/spa/src/QueryManager.cpp
@@ -17,54 +17,54 @@ namespace QPS {
 
     };
 }
-//
-//int main() {
-//    std::vector<QPS::Token> tokens;
-//    QPS::tokenize(std::cin, tokens);
-//    for (QPS::Token t : tokens) {
-//        std::string s = QPS::tokenMap.at(t.tokenType);
-//        std::cout << s + " " << std::endl;
+
+int main() {
+    std::vector<QPS::Token> tokens;
+    QPS::tokenize(std::cin, tokens);
+    for (QPS::Token t : tokens) {
+        std::string s = QPS::tokenMap.at(t.tokenType);
+        std::cout << s + " " << std::endl;
+    }
+
+    std::cout << "Start parsing query" << std::endl;
+    QPS::Container container = QPS::Container(tokens);
+    QPS::parseToken(tokens, container);
+
+    QPS::BasicQueryEvaluator basicQueryEvaluator = QPS::BasicQueryEvaluator();
+    QPS::QueryStruct query = container.getQueryStruct();
+
+//    printf("%s\n", "Query: candidate list");
+//    for (QPS::CandidateStruct candidateStruct : container.getCandidateList()) {
+//        std::cout << QPS::candidateToString(candidateStruct.typeOfCandidate) + ": " + candidateStruct.entityOfCandidate.nameOfEntity  << std::endl;
+//    }
+
+//    std::cout << "Declaration:" << std::endl;
+//    for (auto& it: container.getDeclarationMap()) {
+//        std::cout << it.first << " : " << entityToString(it.second)  << std::endl;
 //    }
 //
-//    std::cout << "Start parsing query" << std::endl;
-//    QPS::Container container = QPS::Container(tokens);
-//    QPS::parseToken(tokens, container);
+//    printf("%s\n", "Query: candidate list");
+//    printf("len: %d\n", query.getCandidateList().size());
+//    for (auto iter : query.getCandidateList()) {
+//        std::cout << iter.entityOfCandidate.nameOfEntity << " " << iter.entityOfCandidate.typeOfEntity << iter.typeOfCandidate << std::endl;
+//    }
 //
-//    QPS::BasicQueryEvaluator basicQueryEvaluator = QPS::BasicQueryEvaluator();
-//    QPS::QueryStruct query = container.getQueryStruct();
+//    printf("%s\n", "Query: Synonym list");
+//    for (auto iter : query.getDeclaredSynonymMap()) {
+//        std::cout << entityToString(iter.second) << " " << iter.first << std::endl;
+//    }
 //
-////    printf("%s\n", "Query: candidate list");
-////    for (QPS::CandidateStruct candidateStruct : container.getCandidateList()) {
-////        std::cout << QPS::candidateToString(candidateStruct.typeOfCandidate) + ": " + candidateStruct.entityOfCandidate.nameOfEntity  << std::endl;
-////    }
+//    printf("len: %d\n", query.getCandidateList().size());
+//    for (auto iter : query.getCandidateList()) {
+//        std::cout << iter.entityOfCandidate.nameOfEntity << iter.entityOfCandidate.typeOfEntity << iter.typeOfCandidate << std::endl;
+//    }
 //
-////    std::cout << "Declaration:" << std::endl;
-////    for (auto& it: container.getDeclarationMap()) {
-////        std::cout << it.first << " : " << entityToString(it.second)  << std::endl;
-////    }
-////
-////    printf("%s\n", "Query: candidate list");
-////    printf("len: %d\n", query.getCandidateList().size());
-////    for (auto iter : query.getCandidateList()) {
-////        std::cout << iter.entityOfCandidate.nameOfEntity << " " << iter.entityOfCandidate.typeOfEntity << iter.typeOfCandidate << std::endl;
-////    }
-////
-////    printf("%s\n", "Query: Synonym list");
-////    for (auto iter : query.getDeclaredSynonymMap()) {
-////        std::cout << entityToString(iter.second) << " " << iter.first << std::endl;
-////    }
-////
-////    printf("len: %d\n", query.getCandidateList().size());
-////    for (auto iter : query.getCandidateList()) {
-////        std::cout << iter.entityOfCandidate.nameOfEntity << iter.entityOfCandidate.typeOfEntity << iter.typeOfCandidate << std::endl;
-////    }
-////
-//    basicQueryEvaluator.evaluateQuery(query);
-////    std::cout << "query status" << query.queryStatus << std::endl;
-////    query.resultTable.printTable();
-////    std::vector<std::string> result;
-////    query.resultTable.getSynonymValue(query.getCandidateList().begin()->entityOfCandidate.nameOfEntity, result);
-////    query.resultTable.printStringVector(result);
-//    QPS::QueryResultProjector queryResultProjector;
-//    queryResultProjector.projectResult(query);
-//}
+    basicQueryEvaluator.evaluateQuery(query);
+//    std::cout << "query status" << query.queryStatus << std::endl;
+//    query.resultTable.printTable();
+//    std::vector<std::string> result;
+//    query.resultTable.getSynonymValue(query.getCandidateList().begin()->entityOfCandidate.nameOfEntity, result);
+//    query.resultTable.printStringVector(result);
+    QPS::QueryResultProjector queryResultProjector;
+    queryResultProjector.projectResult(query);
+}
