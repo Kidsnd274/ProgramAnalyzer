31beba4ec65683638eb3ba9c554f3bb89e74f27b
DESKTOP-LGFS4RO\QMS <e0316059@u.nus.edu>
2022-09-05 05:58:14 +0800

    add test case

---
+++ Team26/Code26/src/unit_testing/src/PKBTest.cpp
@@ -0,0 +1,25 @@
+//
+// Created by QMS on 5/9/2022.
+//
+#include<stdio.h>
+#include "ConstantTable.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting {
+    TEST_CLASS(ConstantTableTest)
+            {
+                    TEST_METHOD_INITIALIZE(ConstantTable_Initializing)
+                    {
+                        ConstantTable ct = new ConstantTable();
+                    }
+
+                    TEST_METHOD(add_retrive_const)
+                    {
+                        ct.insertConst(1);
+                        vector<string> expected;
+                        expected.push_back(std::to_string(1));
+                        Assert::AreEqual(ct.getAllConstants(), expected);
+                    }
+            };
+}
