2d4fdf0bf8bca34ac8991383ba88633f9d167abc
bryans17 <77219972+bryans17@users.noreply.github.com>
2022-09-07 21:08:04 +0800

    Add base ModifiesExtractor

--- Team26/Code26/src/spa/src/sp/de/Extractor.h
+++ Team26/Code26/src/spa/src/sp/de/Extractor.h
@@ -40,6 +40,21 @@ public:
         return val;
     }
 
+    std::vector<int> getAllItemsInStack() {
+        std::stack<int> temp;
+        vector<int> ans;
+        while(!callStack.empty()) {
+            int t = popFromStack();
+            ans.push_back(t);
+            temp.push(t);
+        }
+        while(!temp.empty()) {
+            pushToStack(temp.top());
+            temp.pop();
+        }
+        return ans;
+    }
+
     virtual void extractFromProcedure(std::shared_ptr<ProcedureNode> ptr) = 0;
     virtual void extractFromIf(std::shared_ptr<IfNode> ptr) = 0;
     virtual void extractFromWhile(std::shared_ptr<WhileNode> ptr) = 0;
---
+++ Team26/Code26/src/spa/src/sp/de/ModifiesExtractor.cpp
@@ -0,0 +1,7 @@
+#include "ModifiesExtractor.h"
+
+void extractFromRead(std::shared_ptr<ReadNode> ptr) {
+    string varModified = ptr->getVariableName();
+    //pkb.addModifies(ptr->getStatementNumber(), varModified);
+
+}
