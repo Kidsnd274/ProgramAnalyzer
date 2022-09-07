f68928fafaf437535829b0880d6b5d2150e82e88
bryans17 <77219972+bryans17@users.noreply.github.com>
2022-09-07 21:38:33 +0800

    Add methods for uses for if and while

--- Team26/Code26/src/spa/src/sp/de/UsesExtractor.cpp
+++ Team26/Code26/src/spa/src/sp/de/UsesExtractor.cpp
@@ -1,5 +1,38 @@
-//
-// Created by Bryan Seah on 7/9/22.
-//
-
 #include "UsesExtractor.h"
+
+void UsesExtractor::extractFromIf(std::shared_ptr<IfNode> ptr) {
+    extractFromStatementsWithCond(ptr->getStatementNumber(), ptr->getConditionalExpression());
+}
+
+void UsesExtractor::extractFromWhile(std::shared_ptr<WhileNode> ptr) {
+    extractFromStatementsWithCond(ptr->getStatementNumber(), ptr->getConditionalExpression());
+}
+
+void UsesExtractor::extractFromStatementsWithCond(int stmtNumber, std::shared_ptr<TNode> cond) {
+    pushToStack(stmtNumber);
+    extractFromCond(cond);
+}
+
+void UsesExtractor::extractFromCond(std::shared_ptr<TNode> ptr) {
+    std::unordered_set<std::string> varSet;
+    std::queue<std::shared_ptr<TNode>> q;
+    q.push(ptr);
+
+    while(!q.empty()) {
+        if(q.front()->getNodeType() == NodeType::VariableName) {
+            varSet.insert(q.front()->getValue());
+        }
+        if(q.front()->hasLeftNode()) q.push(q.front()->getLeftNode());
+        if(q.front()->hasRightNode()) q.push(q.front()->getRightNode());
+        q.pop();
+    }
+
+    if(varSet.empty()) return;
+
+    std::vector<int> v = getAllItemsInStack();
+    for(auto i : v) {
+        for(auto &var : varSet) {
+            //pkb.addUses(i, var);
+        }
+    }
+}
--- Team26/Code26/src/spa/src/sp/de/UsesExtractor.h
+++ Team26/Code26/src/spa/src/sp/de/UsesExtractor.h
@@ -1,14 +1,17 @@
-//
-// Created by Bryan Seah on 7/9/22.
-//
+#include "Extractor.h"
+#include <unordered_set>
+#include <queue>
+class UsesExtractor : public Extractor {
+public:
+    UsesExtractor(PKBInterface &pkb) : Extractor(pkb){}
 
-#ifndef SPA_USESEXTRACTOR_H
-#define SPA_USESEXTRACTOR_H
-
-
-class UsesExtractor {
-
+    void extractFromProcedure(std::shared_ptr<ProcedureNode> ptr) override{};
+    void extractFromIf(std::shared_ptr<IfNode> ptr) override;
+    void extractFromWhile(std::shared_ptr<WhileNode> ptr) override;
+    void extractFromRead(std::shared_ptr<ReadNode> ptr) override {};
+    void extractFromPrint(std::shared_ptr<PrintNode> ptr) override;
+    void extractFromAssign(std::shared_ptr<AssignNode> ptr) override;
+    void extractFromCond(std::shared_ptr<TNode> ptr);
+    void extractFromStatementsWithCond(int stmtNumber, std::shared_ptr<TNode> cond);
 };
 
-
-#endif //SPA_USESEXTRACTOR_H
