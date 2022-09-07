750d9541a84a9c11cb2e18b4d0003c97c534001c
bryans17 <77219972+bryans17@users.noreply.github.com>
2022-09-07 21:20:53 +0800

    Add UsesExtractor

---
+++ Team26/Code26/src/spa/src/sp/de/UsesExtractor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bryan Seah on 7/9/22.
+//
+
+#include "UsesExtractor.h"
---
+++ Team26/Code26/src/spa/src/sp/de/UsesExtractor.h
@@ -0,0 +1,14 @@
+//
+// Created by Bryan Seah on 7/9/22.
+//
+
+#ifndef SPA_USESEXTRACTOR_H
+#define SPA_USESEXTRACTOR_H
+
+
+class UsesExtractor {
+
+};
+
+
+#endif //SPA_USESEXTRACTOR_H
