70156da4dcf3f67f894905f59428849f49c55ace
bryans17 <77219972+bryans17@users.noreply.github.com>
2022-09-08 08:51:53 +0800

    Add Parent and Parent*

--- Team26/Code26/src/spa/src/sp/de/ParentExtractor.cpp
+++ Team26/Code26/src/spa/src/sp/de/ParentExtractor.cpp
@@ -1,5 +1,24 @@
-//
-// Created by Bryan Seah on 8/9/22.
-//
-
 #include "ParentExtractor.h"
+
+void ParentExtractor::extractFromWhile(std::shared_ptr<WhileNode> ptr) {
+    std::vector<std::shared_ptr<StatementNode>> stmtList = ptr->getStatementList();
+    extractFromStatementsWithStatementList(ptr->getStatementNumber(), stmtList);
+}
+
+void ParentExtractor::extractFromIf(std::shared_ptr<IfNode> ptr) {
+    std::vector<std::shared_ptr<StatementNode>> stmtList = ptr->getIfStatementList();
+    std::vector<std::shared_ptr<StatementNode>> elseStmtList = ptr->getElseStatementList();
+    stmtList.insert(stmtList.end(), elseStmtList.begin(), elseStmtList.end());
+    extractFromStatementsWithStatementList(ptr->getStatementNumber(), stmtList);
+}
+
+void ParentExtractor::extractFromStatementsWithStatementList(int stmtNum, std::vector<std::shared_ptr<StatementNode>> &stmtList) {
+    pushToStack(stmtNum);
+    std::vector<int> v = getAllItemsInStack();
+    for (auto &stmt : stmtList) {
+        //pkb.addParent(v.top(), stmt->getStatementNumber());
+        for (auto p : v) {
+            //pkb.addParentStar(p, stmt->getStatementNumber());
+        }
+    }
+}
