a13bdcc79bdc0cabf2588d8ca9aa4cd70b4e55c7
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-09-10 23:05:08 +0800

    Add comments on coding issues

--- Team26/Code26/src/unit_testing/src/sp/stubs/PKBInterfaceStubForDE.h
+++ Team26/Code26/src/unit_testing/src/sp/stubs/PKBInterfaceStubForDE.h
@@ -5,6 +5,8 @@ public:
     std::unordered_map<T, Y> unorderedMap;
 
     void addFollows(int stmt1Number, int stmt2Number) override;
+    // TODO: Is there a way to override without making the base method virtual
+    // TODO: Having issues overriding the base method with the methods at the btm
 //    void addModifies(int stmtNumber, std::string varModified) override;
 //    void addModifies(std::string procedureName, std::string varModified) override;
 //    void addParent(int parentStmtNumber, int stmtNumber) override;
