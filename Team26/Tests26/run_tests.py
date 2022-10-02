81d85021ce5b991b6a71ce2049c5c75b97fb5dd8
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-10-03 02:17:25 +0800

    Improve run_tests script
    Does not stop testing if one test case fails
    Reorganize log and out.xml files when testcase fails

--- Team26/Tests26/run_tests.py
+++ Team26/Tests26/run_tests.py
@@ -4,19 +4,20 @@ import hashlib
 import sys
 import os
 
+testdir = pathlib.Path(__file__).parent.absolute()
+codedir = testdir.parent / "Code26"
+outxml = testdir / "out.xml"
+logfile = testdir / "run_tests.log"
+
 
 class TestCase:
     def __init__(self, name, folder):
         self.name = name
         self.source_file = folder / (name + "_source.txt")
         self.queries_file = folder / (name + "_queries.txt")
-        self.out_file = folder / (name + "_out.xml")
+        self.out_file_failed = testdir / "failed_tests" / (name + "_out.xml")
+        self.log_file_failed = testdir / "failed_tests" / (name + ".log")
 
-
-testdir = pathlib.Path(__file__).parent.absolute()
-codedir = testdir.parent / "Code26"
-outxml = testdir / "out.xml"
-logfile = testdir / "run_tests.log"
 
 print(testdir)
 
@@ -49,27 +50,18 @@ if not found:
     exit(1)
 
 
-def cmp_hash(file1, file2):
-    hash1 = hashlib.sha256()
-    hash2 = hashlib.sha256()
+def move_file(curr_file, new_location):
+    if new_location.exists():
+        new_location.unlink()
+    curr_file.rename(new_location)
 
-    f1 = open(file1, 'rb')
-    while True:
-        data1 = f1.read()
-        if not data1:
-            break
-        hash1.update(data1)
-    f1.close()
 
-    f2 = open(file2, 'rb')
-    while True:
-        data2 = f2.read()
-        if not data2:
-            break
-        hash2.update(data2)
-    f2.close()
-
-    return hash1 == hash2
+def move_failed_files(testcase, out_file_also):
+    if not ((testdir / "failed_tests").exists and (testdir / "failed_tests").is_dir()):
+        pathlib.Path.mkdir(testdir / "failed_tests")
+    move_file(logfile, testcase.log_file_failed)
+    if out_file_also:
+        move_file(outxml, testcase.out_file_failed)
 
 
 def if_passed(result):
@@ -90,21 +82,26 @@ def run_test(testcase):
     return process.returncode
 
 
-def run_multiple_tests(tests, milestone_name):
-    for testcase in tests:
-        print("Running testcase " + testcase.name)
+failed_tests = []
+
+
+def run_multiple_tests(selected_tests, milestone_name):
+    for testcase in selected_tests:
+        print("Running testcase " + testcase.name + "...", end="")
         completed_process = run_test(testcase)
         if completed_process != 0:
-            print("ERROR: Testcase " + testcase.name + " in " + milestone_name + " failed to run")
-            print("Check run_tests.log!")
-            exit(1)
-        # if not cmp_hash(outxml, testcase.out_file):
-        if not if_passed(outxml):
-            print("ERROR: Failed testcase " + testcase.name + " in " + milestone_name)
-            print("Check run_tests.log and out.xml!")
-            exit(1)
+            failed_tests.append(testcase)
+            move_failed_files(testcase, False)
+            print(" \U0000274C Failed to run")
+        elif not if_passed(outxml):
+            failed_tests.append(testcase)
+            move_failed_files(testcase, True)
+            print(" \U0000274C Failed testcase.")
+        else:
+            print(" \U00002714")
 
 
+# Edit test cases here
 print("-----------------------SPRINT 1-----------------------")
 sprint1 = testdir / "sprint1"
 tests = [TestCase("test_sprint1", sprint1)]
@@ -120,4 +117,14 @@ tests = [TestCase("milestone1_test1", milestone1),
 
 run_multiple_tests(tests, "Milestone 1")
 
-print("JOB'S DONE")
+
+print("")
+if failed_tests:
+    with open("failed_tests.txt", "w") as f:
+        for cases in failed_tests:
+            f.write(cases.name)
+            f.write("\n")
+    print("Test cases failed \U0000274C Check failed_tests.txt for the list and failed_tests folder for log and out.xml")
+    exit(1)
+else:
+    print("JOB'S DONE \U00002705")
