4d540124af3b9e5c056f22af05005cba108ded87
Li-Lehao <e0492462@u.nus.edu>
2022-10-03 22:45:41 +0800

    Integration.

---
+++ Team26/Code26/src/spa/src/qps/type/Follows.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 乐灏 Cheese on 2022/10/3.
+//
+
+#include "Follows.hgit "
---
+++ Team26/Code26/src/spa/src/qps/type/Follows.h
@@ -0,0 +1,10 @@
+#ifndef SPA_FOLLOWS_H
+#define SPA_FOLLOWS_H
+
+#include "RelationClause.h"
+
+class Follows : RelationClause {
+};
+
+
+#endif //SPA_FOLLOWS_H
