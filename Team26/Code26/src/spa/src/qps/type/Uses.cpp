285e71db6f74220d0fd2fc4e5b88ed2a509f2349
Li-Lehao <e0492462@u.nus.edu>
2022-10-09 23:00:06 +0800

    Unit test Uses. Fix bug in Uses.

--- Team26/Code26/src/spa/src/qps/type/Uses.cpp
+++ Team26/Code26/src/spa/src/qps/type/Uses.cpp
@@ -1,60 +1,73 @@
-//#include "Uses.h"
-//
-//const std::unordered_map<ArgumentType, std::unordered_set<ArgumentType>> Uses::validationTable {
-//        {
-//            ASSIGN_SYNONYM,
-//            {
-//                VAR_SYNONYM,
-//                ACTUAL_NAME
-//            }
-//        },
-//        {
-//            PRINT_SYNONYM,
-//            {
-//                VAR_SYNONYM,
-//                ACTUAL_NAME
-//            }
-//        },
-//        {
-//            IF_SYNONYM,
-//            {
-//                VAR_SYNONYM,
-//                ACTUAL_NAME
-//            }
-//        },
-//        {
-//            WHILE_SYNONYM,
-//            {
-//                VAR_SYNONYM,
-//                ACTUAL_NAME
-//            }
-//        },
-//        {
-//            CALL_SYNONYM,
-//            {
-//                VAR_SYNONYM,
-//                ACTUAL_NAME
-//            }
-//        },
-//        {
-//            PROCEDURE_SYNONYM,
-//            {
-//                VAR_SYNONYM,
-//                ACTUAL_NAME
-//            }
-//        },
-//        {
-//            NUMBER, // Line number of statement
-//            {
-//                VAR_SYNONYM,
-//                ACTUAL_NAME
-//            }
-//        },
-//        {
-//            ACTUAL_NAME,    // Actual name of procedure, i.e. "prod1"
-//            {
-//                VAR_SYNONYM,
-//                ACTUAL_NAME
-//            }
-//        }
-//};
+#include "Uses.h"
+
+const std::unordered_map<
+        Argument::ArgumentType,
+        std::unordered_set<Argument::ArgumentType>
+        > Uses::usesValidationTable {
+        {
+            Argument::ASSIGN_SYNONYM,
+            {
+                Argument::VAR_SYNONYM,
+                Argument::ACTUAL_NAME
+            }
+        },
+        {
+            Argument::PRINT_SYNONYM,
+            {
+                Argument::VAR_SYNONYM,
+                Argument::ACTUAL_NAME
+            }
+        },
+        {
+            Argument::IF_SYNONYM,
+            {
+                Argument::VAR_SYNONYM,
+                Argument::ACTUAL_NAME
+            }
+        },
+        {
+            Argument::WHILE_SYNONYM,
+            {
+                Argument::VAR_SYNONYM,
+                Argument::ACTUAL_NAME
+            }
+        },
+        {
+            Argument::CALL_SYNONYM,
+            {
+                Argument::VAR_SYNONYM,
+                Argument::ACTUAL_NAME
+            }
+        },
+        {
+            Argument::PROCEDURE_SYNONYM,
+            {
+                Argument::VAR_SYNONYM,
+                Argument::ACTUAL_NAME
+            }
+        },
+        {
+            Argument::NUMBER, // Line number of statement
+            {
+                Argument::VAR_SYNONYM,
+                Argument::ACTUAL_NAME
+            }
+        },
+        {
+            Argument::ACTUAL_NAME,    // Actual name of procedure, i.e. "prod1"
+            {
+                Argument::VAR_SYNONYM,
+                Argument::ACTUAL_NAME
+            }
+        }
+};
+
+bool Uses::isValid() {
+    auto iter = Uses::usesValidationTable.find(this->argument1.argumentType);
+    if (iter != Uses::usesValidationTable.end()) {
+        if (iter->second.find(this->argument2.argumentType) != iter->second.end()) {
+            return true;
+        }
+    }
+    return false;
+}
