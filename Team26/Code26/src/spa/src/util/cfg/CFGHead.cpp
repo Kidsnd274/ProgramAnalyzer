57225b0daa6db16daa0db3236a07247b70fbfe9a
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-10-10 03:29:08 +0800

    Change CFG Map types
    Double check what's going on in == operator as it is causing some issues
    Can't compile as well. Update test cases with the new map type

--- Team26/Code26/src/spa/src/util/cfg/CFGHead.cpp
+++ Team26/Code26/src/spa/src/util/cfg/CFGHead.cpp
@@ -3,10 +3,14 @@
 #include <utility>
 
 void CFGHead::connectNode(CFGNode node1, CFGNode node2)  {
+    STMT_NUM stmtNum = node1.getStmtNumber();
     if (node1.isDummyNode()) {
-        dummyAdjList.insert({node1.getStmtNumber(), node2});
+        dummyAdjList.insert({stmtNum, node2});
     } else {
-        adjList.insert({node1.getStmtNumber(), node2});
+        if (adjList.find(stmtNum) == adjList.end()) { // If key doesn't exist
+            adjList[stmtNum] = EDGES(); // Initialize key with null vector
+        }
+        adjList[stmtNum].push_back(node2);
     }
 }
 
@@ -14,26 +18,14 @@ EDGES CFGHead::getEdges(STMT_NUM stmtNumber) {
     if (adjList.find(stmtNumber) == adjList.end()) {
         return {};
     }
-    EDGES result;
-    auto variables = this->adjList.equal_range(stmtNumber);
-
-    for (auto it = variables.first; it != variables.second; ++it) {
-        result.push_back(it->second);
-    }
-    return result;
+    return adjList.at(stmtNumber);
 }
 
-EDGES CFGHead::getDummyNodeEdges(STMT_NUM stmtNumber) {
+CFGNode CFGHead::getDummyNodeEdge(STMT_NUM stmtNumber) {
     if (dummyAdjList.find(stmtNumber) == dummyAdjList.end()) {
-        return {};
+        return {-1, CFGNodeType::NullNode};
     }
-    EDGES result;
-    auto variables = this->dummyAdjList.equal_range(stmtNumber);
-
-    for (auto it = variables.first; it != variables.second; ++it) {
-        result.push_back(it->second);
-    }
-    return result;
+    return dummyAdjList.at(stmtNumber);
 }
 
 CFGHeadPtr CFGHead::createNewCFG() {
@@ -44,14 +36,63 @@ CFGMap CFGHead::getNormalNodeMap() {
     return this->adjList;
 }
 
-CFGMap CFGHead::getDummyNodeMap() {
+CFGDummyMap CFGHead::getDummyNodeMap() {
     return this->dummyAdjList;
 }
 
+bool CFGHead::compareEdgesEquality(EDGES &v1, EDGES &v2) {
+    if (v1.size() != v2.size()) {
+        return false;
+    }
+//    std::sort(v1.begin(), v1.end());
+//    std::sort(v2.begin(), v2.end());
+    return v1 == v2;
+}
+
 bool operator== (CFGHead leftCFG, CFGHead rightCFG) {
-    bool cond1 = leftCFG.getNormalNodeMap() == rightCFG.getNormalNodeMap();
-    bool cond2 = leftCFG.getDummyNodeMap() == rightCFG.getDummyNodeMap();
-    return cond1 && cond2;
+    std::unordered_set<STMT_NUM> verifiedNodes;
+
+    auto leftNodeMap = leftCFG.getNormalNodeMap();
+    auto rightNodeMap = rightCFG.getNormalNodeMap();
+
+    // Check that items in left CFG is equal right CFG
+    for (auto it = leftNodeMap.begin(); it != leftNodeMap.end();) {
+        auto const& key = it->first;
+        if (rightNodeMap.find(key) == rightNodeMap.end()) { // Key not found in right node
+            return false;
+        }
+        EDGES leftEdges = leftNodeMap.at(key);
+        EDGES rightEdges = rightNodeMap.at(key);
+//        bool compareEdges = CFGHead::compareEdgesEquality(leftEdges, rightEdges);
+//        if (!compareEdges) {
+//            return false;
+//        }
+        verifiedNodes.insert(key);
+        while (++it != leftNodeMap.end() && it->first == key);
+    }
+
+    // Check that items in the right CFG is equal left CFG (in case right has more)
+    for (auto it = rightNodeMap.begin(); it != rightNodeMap.end();) {
+        auto const& key = it->first;
+        if (verifiedNodes.find(key) != verifiedNodes.end()) { // Verified stmt number already
+            while (++it != leftNodeMap.end() && it->first == key);
+            continue;
+        }
+        if (leftNodeMap.find(key) == leftNodeMap.end()) { // Key not found in right node
+            return false;
+        }
+        EDGES leftEdges = leftNodeMap.at(key);
+        EDGES rightEdges = rightNodeMap.at(key);
+//        bool compareEdges = CFGHead::compareEdgesEquality(leftEdges, rightEdges);
+//        if (!compareEdges) {
+//            return false;
+//        }
+        while (++it != leftNodeMap.end() && it->first == key);
+    }
+
+    // Compare DummyNodes
+//    bool cond2 = leftCFG.getDummyNodeMap() == rightCFG.getDummyNodeMap();
+    return true;
 }
 
 bool operator!= (CFGHead leftCFG, CFGHead rightCFG) {
--- Team26/Code26/src/spa/src/util/cfg/CFGHead.h
+++ Team26/Code26/src/spa/src/util/cfg/CFGHead.h
@@ -2,7 +2,9 @@
 #define SPA_CFGHEAD_H
 
 #include "CFGNode.h"
+#include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 class CFGHead;
@@ -10,20 +12,21 @@ class CFGHead;
 typedef std::shared_ptr<CFGHead> CFGHeadPtr;
 typedef int STMT_NUM;
 typedef std::vector<CFGNode> EDGES;
-typedef std::vector<std::vector<EDGES>> CFGMap;
-typedef std::unordered_map<STMT_NUM, CFGNode> CFGAdjMap;
+typedef std::unordered_map<STMT_NUM, EDGES> CFGMap;
+typedef std::unordered_map<STMT_NUM, CFGNode> CFGDummyMap;
 
 class CFGHead {
 private:
-     CFGMap adjList;
-     CFGAdjMap dummyAdjList;
+    CFGMap adjList;
+    CFGDummyMap dummyAdjList;
 public:
     void connectNode(CFGNode node1, CFGNode node2);
     EDGES getEdges(STMT_NUM stmtNumber);
-    EDGES getDummyNodeEdges(STMT_NUM stmtNumber);
+    CFGNode getDummyNodeEdge(STMT_NUM stmtNumber);
     CFGMap getNormalNodeMap();
-    CFGMap getDummyNodeMap();
+    CFGDummyMap getDummyNodeMap();
     static CFGHeadPtr createNewCFG();
+    static bool compareEdgesEquality(EDGES& v1, EDGES& v2);
 };
 
 bool operator== (CFGHead leftCFG, CFGHead rightCFG);
