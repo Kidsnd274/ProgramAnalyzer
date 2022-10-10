41671017a38a298377262ee464965a28b86caa46
Li-Lehao <e0492462@u.nus.edu>
2022-10-10 19:05:29 +0800

    Implement WithClause.

--- Team26/Code26/src/spa/src/qps/type/WithClause.h
+++ Team26/Code26/src/spa/src/qps/type/WithClause.h
@@ -11,13 +11,20 @@ enum AttributeType {
     stmtLineNumber
 };
 
-class WithClause {
+class WithClause : public Clause {
 protected:
     struct WithClauseArgument {
         Argument::ArgumentType argumentType;
+        AttributeType attributeType;
+        std::string attributeValue;
+    } arg1, arg2;
 
-    };
+    WithClause(WithClauseArgument arg1, WithClauseArgument arg2) {
+        this->arg1 = arg1;
+        this->arg2 = arg2;
+    }
+
+    bool isValid() override;
 };
-
 
 #endif //SPA_WITHCLAUSE_H
