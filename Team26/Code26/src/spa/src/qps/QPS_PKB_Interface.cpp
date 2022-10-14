d59d65056777430f87fae9b911c4aa44b491bcac
NoraYUuu <norafish79@gmail.com>
2022-10-14 21:51:57 +0800

    debug build

--- Team26/Code26/src/spa/src/qps/QPS_PKB_Interface.cpp
+++ Team26/Code26/src/spa/src/qps/QPS_PKB_Interface.cpp
@@ -8,28 +8,40 @@ std::shared_ptr<AssignNode> getAssignTNode(std::string assignRef) {
     return QPS_PKB_Interface::pkbInterface->getAssignTNode(assignRef);
 }
 
-std::unordered_map<int, vector<int>> QPS_PKB_Interface::getAllParentRelations() {}
+std::unordered_map<int, vector<int>> QPS_PKB_Interface::getAllParentRelations() {
+    return {};
+}
 
 std::unordered_map<std::string, std::vector<std::string>> QPS_PKB_Interface::getAllCallRelations() {
 //    return QPS_PKB_Interface::pkbInterface->getAllCall();
+    return {};
 }
 
 std::unordered_map<std::string, std::vector<std::string>> QPS_PKB_Interface::getAllCallTRelations() {
     //    return QPS_PKB_Interface::pkbInterface->getAllCallStar();
+    return {};
 }
 
 std::unordered_map<std::string, std::vector<std::string>> QPS_PKB_Interface::getAllUsesProcRelations() {
-
+    return {};
 }
 
 std::unordered_map<std::string, std::vector<std::string>> QPS_PKB_Interface::getAllModifiesProcRelations() {
-
+    return {};
 }
 
-std::unordered_map<int, std::vector<std::string>> QPS_PKB_Interface::getAllUsesRelations() {}
+std::unordered_map<int, std::vector<std::string>> QPS_PKB_Interface::getAllUsesRelations() {
+    return {};
+}
 
-std::unordered_map<int, std::vector<std::string>> QPS_PKB_Interface::getAllModifiesRelations() {}
+std::unordered_map<int, std::vector<std::string>> QPS_PKB_Interface::getAllModifiesRelations() {
+    return {};
+}
 
-std::unordered_map<int, vector<int>> QPS_PKB_Interface::getAllParentTRelations() {}
+std::unordered_map<int, vector<int>> QPS_PKB_Interface::getAllParentTRelations() {
+    return {};
+}
 
-std::unordered_map<int, int> QPS_PKB_Interface::getAllFollowsRelations() {}
+std::unordered_map<int, int> QPS_PKB_Interface::getAllFollowsRelations() {
+    return {};
+}
