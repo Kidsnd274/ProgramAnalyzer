6a1f540500f1d8da48ffb667ebb7c790bd366218
Li-Lehao <e0492462@u.nus.edu>
2022-10-17 01:01:26 +0800

    Fix bug in withClauseEvaluator

--- Team26/Code26/src/spa/src/qps/evaluator/WithClauseEvaluator.cpp
+++ Team26/Code26/src/spa/src/qps/evaluator/WithClauseEvaluator.cpp
@@ -4,7 +4,7 @@ void WithClauseEvaluator::evaluate(QPS::ResultTable *resultTable) {
     std::vector<std::vector<std::string>> table = resultTable->getTable();
     WithClause::WithClauseArgument arg1 = this->withClause->arg1;
     WithClause::WithClauseArgument arg2 = this->withClause->arg2;
-    if (arg2.argument.argumentType == Argument::ACTUAL_NAME) { // a.attribute = "x"
+    if (arg2.argument.argumentType == Argument::ACTUAL_NAME || arg2.argument.argumentType == Argument::NUMBER) { // a.attribute = "x"
         int col = resultTable->getSynonymColRef().find(arg1.argument.argumentName)->second;
         for (int i = table.size() - 1; i >= 0; i--) {
             if (table.at(i).at(col) != arg2.argument.argumentName) {
