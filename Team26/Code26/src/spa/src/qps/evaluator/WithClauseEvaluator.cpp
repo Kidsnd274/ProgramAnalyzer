d3f6cb0517e989f0b699e373e9aba10a73dde29d
Feng Zhunyi <leofengbar@gmail.com>
2022-10-17 02:49:58 +0800

    QPS: fix multi clause "and" issue

--- Team26/Code26/src/spa/src/qps/evaluator/WithClauseEvaluator.cpp
+++ Team26/Code26/src/spa/src/qps/evaluator/WithClauseEvaluator.cpp
@@ -5,15 +5,27 @@ void WithClauseEvaluator::evaluate(QPS::ResultTable *resultTable) {
     WithClause::WithClauseArgument arg1 = this->withClause->arg1;
     WithClause::WithClauseArgument arg2 = this->withClause->arg2;
     if (arg2.argument.argumentType == Argument::ACTUAL_NAME || arg2.argument.argumentType == Argument::NUMBER) { // a.attribute = "x"
-        int col = resultTable->getSynonymColRef().find(arg1.argument.argumentName)->second;
+        auto iter = resultTable->getSynonymColRef().find(arg1.argument.argumentName);
+        if (iter == resultTable->getSynonymColRef().end()) {
+            return;
+        }
+        int col = iter->second;
         for (int i = table.size() - 1; i >= 0; i--) {
             if (table.at(i).at(col) != arg2.argument.argumentName) {
                 resultTable->deleteRowFromTable(i);
             }
         }
     } else { // a.attribute = b.attribute
-        int col1 = resultTable->getSynonymColRef().find(arg1.argument.argumentName)->second;
-        int col2 = resultTable->getSynonymColRef().find(arg2.argument.argumentName)->second;
+        auto iter1 = resultTable->getSynonymColRef().find(arg1.argument.argumentName);
+        if (iter1 == resultTable->getSynonymColRef().end()) {
+            return;
+        }
+        int col1 = iter1->second;
+        auto iter2 = resultTable->getSynonymColRef().find(arg2.argument.argumentName);
+        if (iter2 == resultTable->getSynonymColRef().end()) {
+            return;
+        }
+        int col2 = iter2->second;
         for (int i = table.size() - 1; i >= 0; i--) {
             if (table.at(i).at(col1) != table.at(i).at(col2)) {
                 resultTable->deleteRowFromTable(i);
