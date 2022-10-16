7183e914e73ddb5fcbf9613a86c307cebb93a33d
Feng Zhunyi <leofengbar@gmail.com>
2022-10-16 16:16:58 +0800

    QPS: parser bug fix

--- Team26/Code26/src/spa/src/qps/QueryManager.cpp
+++ Team26/Code26/src/spa/src/qps/QueryManager.cpp
@@ -169,6 +169,7 @@ void QueryManager::handleQuery(PKBInterface *pkb, std::string queryString, std::
             query.setStatus(VALID_QUERY);;
         }
     }
+    query.validate();
     QueryEvaluator::evaluate(&query); // Call QueryEvaluator to evaluate the query. Store the result in query.resultTable.
 
     QueryResultProjector* queryResultProjector = new QueryResultProjector();
