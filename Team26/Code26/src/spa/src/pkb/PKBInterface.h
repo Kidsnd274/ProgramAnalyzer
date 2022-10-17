c65aafcfb65565937d15007f34e756508cc3eb74
DESKTOP-LGFS4RO\QMS <e0316059@u.nus.edu>
2022-10-17 12:20:43 +0800

    delete unused file

--- Team26/Code26/src/spa/src/pkb/PKBInterface.h
+++ Team26/Code26/src/spa/src/pkb/PKBInterface.h
@@ -58,16 +58,16 @@ public:
     bool existRelation(const RelationStruct& relation);
     std::shared_ptr<AssignNode> getAssignTNode(const string& assignRef);
 
-    virtual unordered_map<std::string, std::vector<std::string>> getAllCall();
-    virtual unordered_map<std::string, std::vector<std::string>> getAllCallStar();
-    virtual unordered_map<int,int> getAllFollow();
-    virtual unordered_map<int,int> getAllFollowStar();
-    virtual unordered_map<int, std::vector<std::string>> getAllModifyByStmt();
-    virtual unordered_map<std::string, std::vector<std::string>> getAllModifyByProc();
-    virtual unordered_map<int, std::vector<int>> getAllParent();
-    virtual unordered_map<int, std::vector<int>> getAllParentStar();
-    virtual unordered_map<int, std::vector<std::string>> getAllUseByStmt();
-    virtual unordered_map<std::string, std::vector<std::string>> getAllUseByProc();
+    virtual std::unordered_map<std::string, std::vector<std::string>> getAllCall();
+    virtual std::unordered_map<std::string, std::vector<std::string>> getAllCallStar();
+    virtual std::unordered_map<int,int> getAllFollow();
+    virtual std::unordered_map<int,int> getAllFollowStar();
+    virtual std::unordered_map<int, std::vector<std::string>> getAllModifyByStmt();
+    virtual std::unordered_map<std::string, std::vector<std::string>> getAllModifyByProc();
+    virtual std::unordered_map<int, std::vector<int>> getAllParent();
+    virtual std::unordered_map<int, std::vector<int>> getAllParentStar();
+    virtual std::unordered_map<int, std::vector<std::string>> getAllUseByStmt();
+    virtual std::unordered_map<std::string, std::vector<std::string>> getAllUseByProc();
     virtual std::vector<vector<Statement>> getAllStmtLists();
     virtual std::string getProcLineNumberByName(std::string procName);
     virtual std::string getCallProcName(std::string callLineNumber);
