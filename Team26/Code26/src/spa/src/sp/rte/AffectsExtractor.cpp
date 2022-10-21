7ee9cb9c8af4cf9a81127d8550a9c09f6e51d14f
bryans17 <77219972+bryans17@users.noreply.github.com>
2022-10-21 10:28:26 +0800

    Add some stuff

--- Team26/Code26/src/spa/src/sp/rte/AffectsExtractor.cpp
+++ Team26/Code26/src/spa/src/sp/rte/AffectsExtractor.cpp
@@ -1,2 +1,10 @@
 
 #include "AffectsExtractor.h"
+
+void computeAffects(CFGHeadPtr cfg, PKBInterface* pkb, STMT_NUM stmt) {
+//    if(pkb->hasAffects(stmt)) {
+//        return;
+//    }
+//
+//    string varModified = pkb->getModifiedVariable(stmt);
+}
