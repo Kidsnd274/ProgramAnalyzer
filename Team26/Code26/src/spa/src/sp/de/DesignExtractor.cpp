42f45afa33711342fe88b3bf102401336aedf079
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-11-01 15:29:36 +0800

    I broke stuff

--- Team26/Code26/src/spa/src/sp/de/DesignExtractor.cpp
+++ Team26/Code26/src/spa/src/sp/de/DesignExtractor.cpp
@@ -31,27 +31,7 @@ void DesignExtractor::addCallStatement(CallStruct &c) {
 void DesignExtractor::extract(std::vector<std::shared_ptr<ProcedureNode>> procedures) {
     for(auto p : procedures) {
         for (auto &e: extractorList) {
-            traverse(p, e);
-        }
-    }
-    CallStatementHandler callStatementHandler(callList, nameToIndex, indexToName);
-    callStatementHandler.handleCalls(pkbInterface);
-}
-
-void DesignExtractor::traverse(std::shared_ptr<ProcedureNode> ptr, std::shared_ptr<Extractor> e) {
-    e->setProcedureName(ptr->getProcedureName());
-    std::stack<int> s;
-    e->setStack(s);
-    e->extractFromProcedure(ptr);
-    std::vector<std::shared_ptr<StatementNode>> stmtList = ptr->getStatementList();
-    for (auto stmt : stmtList) {
-        traverse(stmt, e);
-    }
-}
-
-void DesignExtractor::traverse(std::shared_ptr<StatementNode> ptr, std::shared_ptr<Extractor> e) {
-    if (ptr->getStatementType() == statementType::IF) {
-        std::shared_ptr<IfNode> t = std::static_pointer_cast<IfNode>(ptr);
+            travers_pointer_cast<IfNode>(ptr);
         e->extractFromIf(t);
 
         std::vector<std::shared_ptr<StatementNode>> stmtList = t->getIfStatementList();
