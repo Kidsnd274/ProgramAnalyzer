5d3e26b0e8f50a820bda4d2a2fd1a1090d6085d9
bryans17 <77219972+bryans17@users.noreply.github.com>
2022-11-02 13:07:14 +0800

    Rename test files

--- Team26/Tests26/run_tests.py
+++ Team26/Tests26/run_tests.py
@@ -143,7 +143,7 @@ run_multiple_tests(tests)
 
 print("-----------------------SPRINT 5 / MILESTONE 3-----------------------")
 milestone3 = testdir / "milestone3"
-tests = [TestCase("milestone3_test_sam", milestone3)]
+tests = [TestCase("milestone3_test1", milestone3)]
 
 run_multiple_tests(tests)
 
