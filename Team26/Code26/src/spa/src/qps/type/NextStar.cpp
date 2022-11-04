da7c3b938d740f482f6f969e12f296536d8a7826
Li-Lehao <e0492462@u.nus.edu>
2022-11-04 15:02:45 +0800

    Fix bug NextStar.

--- Team26/Code26/src/spa/src/qps/type/NextStar.cpp
+++ Team26/Code26/src/spa/src/qps/type/NextStar.cpp
@@ -4,6 +4,12 @@
 
 const std::unordered_set<Argument::ArgumentType> secondARGs = {
         Argument::STMT_SYNONYM,
+        Argument::IF_SYNONYM,
+        Argument::WHILE_SYNONYM,
+        Argument::READ_SYNONYM,
+        Argument::PRINT_SYNONYM,
+        Argument::CALL_SYNONYM,
+        Argument::ASSIGN_SYNONYM,
         Argument::NUMBER,
         Argument::WILDCARD,
 };
@@ -12,18 +18,15 @@ const std::unordered_map<
         Argument::ArgumentType,
         std::unordered_set<Argument::ArgumentType>
 > NextStar::nextStarValidationTable {
-        {
-                Argument::WILDCARD,
-                secondARGs
-        },
-        {
-                Argument::NUMBER,
-                secondARGs
-        },
-        {
-                Argument::STMT_SYNONYM,
-                secondARGs
-        },
+        {Argument::STMT_SYNONYM, secondARGs},
+        {Argument::IF_SYNONYM, secondARGs},
+        {Argument::WHILE_SYNONYM, secondARGs},
+        {Argument::READ_SYNONYM, secondARGs},
+        {Argument::PRINT_SYNONYM, secondARGs},
+        {Argument::CALL_SYNONYM, secondARGs},
+        {Argument::ASSIGN_SYNONYM, secondARGs},
+        {Argument::NUMBER, secondARGs},
+        {Argument::WILDCARD, secondARGs}
 };
 
 bool NextStar::isValid() {
