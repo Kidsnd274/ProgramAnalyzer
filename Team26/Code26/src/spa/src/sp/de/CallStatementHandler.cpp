295519cca9e4d54e54a2638c7026b20f5344f97b
bryans17 <77219972+bryans17@users.noreply.github.com>
2022-11-06 19:49:08 +0800

    Refactor

--- Team26/Code26/src/spa/src/sp/de/CallStatementHandler.cpp
+++ Team26/Code26/src/spa/src/sp/de/CallStatementHandler.cpp
@@ -1,5 +1,49 @@
 #include "CallStatementHandler.h"
 
+void CallStatementHandler::checkIfProcedureExist(std::string& procedureCalled) {
+    if (nameToIndex.find(procedureCalled) == nameToIndex.end()) {
+        throw SemanticErrorException("Call to non-existent procedure " + procedureCalled);
+    }
+}
+
+void CallStatementHandler::checkCyclicProcedureCall(std::string& procedureCalled) {
+    if (nameToIndex[procedureCalled].second == 1) {
+        throw SemanticErrorException("Cyclic procedure calls in " + procedureCalled);
+    }
+}
+
+void CallStatementHandler::addToPkb(CallStruct& cs, PKBInterface *pkb, std::string& procedureName) {
+    // now add all modified and used variable to procedure and parent*
+    std::unordered_set<std::string> modified = pkb->getAllVariablesModified(cs.getProcedureCalled());
+    std::unordered_set<std::string> used = pkb->getAllVariablesUsed(cs.getProcedureCalled());
+    std::unordered_set<int> parentStar = pkb->getParentStar(cs.getStatementNumber());
+    std::unordered_set<std::string> callStar = pkb->getCallStar(cs.getProcedureCalled());
+
+    for (auto &var : modified) {
+        pkb->addModifies(procedureName, var);
+        pkb->addModifies(cs.getStatementNumber(), var);
+        for (auto p : parentStar) {
+            pkb->addModifies(p, var);
+        }
+    }
+
+    for (auto &var : used) {
+        pkb->addUses(procedureName, var);
+        pkb->addUses(cs.getStatementNumber(), var);
+        for (auto p : parentStar) {
+            pkb->addUses(p, var);
+        }
+    }
+
+    // handle calls and callstar
+    for (auto &proc : callStar) {
+        pkb->addCallStar(procedureName, proc);
+    }
+
+    pkb->addCallStar(procedureName, cs.getProcedureCalled());
+    pkb->addCall(procedureName, cs.getProcedureCalled());
+}
+
 void CallStatementHandler::handleCalls(PKBInterface *pkb) {
     for (int i = 0; i < callList.size(); ++i) {
         dfs(i, pkb);
@@ -22,47 +66,14 @@ void CallStatementHandler::dfs(int i, PKBInterface *pkb) {
     for (int j = 0; j < callList[i].size(); ++j) {
         std::string procedureCalled = callList[i][j].getProcedureCalled();
 
-        if (nameToIndex.find(procedureCalled) == nameToIndex.end()) {
-            throw SemanticErrorException("Call to non-existent procedure " + procedureCalled);
-        }
-
-        if (nameToIndex[procedureCalled].second == 1) {
-            throw SemanticErrorException("Cyclic procedure calls in " + procedureCalled);
-        }
+        checkIfProcedureExist(procedureCalled);
+        checkCyclicProcedureCall(procedureCalled);
 
         if (nameToIndex[procedureCalled].second == 0) {
             dfs(nameToIndex[procedureCalled].first, pkb);
         }
 
-        // now add all modified and used variable to procedure and parent*
-        std::unordered_set<std::string> modified = pkb->getAllVariablesModified(callList[i][j].getProcedureCalled());
-        std::unordered_set<std::string> used = pkb->getAllVariablesUsed(callList[i][j].getProcedureCalled());
-        std::unordered_set<int> parentStar = pkb->getParentStar(callList[i][j].getStatementNumber());
-        std::unordered_set<std::string> callStar = pkb->getCallStar(callList[i][j].getProcedureCalled());
-
-        for (auto &var : modified) {
-            pkb->addModifies(indexToName[i], var);
-            pkb->addModifies(callList[i][j].getStatementNumber(), var);
-            for (auto p : parentStar) {
-                pkb->addModifies(p, var);
-            }
-        }
-
-        for (auto &var : used) {
-            pkb->addUses(indexToName[i], var);
-            pkb->addUses(callList[i][j].getStatementNumber(), var);
-            for (auto p : parentStar) {
-                pkb->addUses(p, var);
-            }
-        }
-
-        // handle calls and callstar
-        for (auto &proc : callStar) {
-            pkb->addCallStar(indexToName[i], proc);
-        }
-
-        pkb->addCallStar(indexToName[i], procedureCalled);
-        pkb->addCall(indexToName[i], procedureCalled);
+        addToPkb(callList[i][j], pkb, indexToName[i]);
     }
 
     nameToIndex[indexToName[i]].second = 2;
